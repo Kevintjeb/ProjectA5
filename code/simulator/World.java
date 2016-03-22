9c34cdb8b7e8f21918f3bd989460b21124748436
Kevin van den Broek <kevin.vandenbroek@home.nl>
2016-03-22 12:37:03 +0100

    world update - json

--- code/simulator/World.java
+++ code/simulator/World.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Map;
@@ -19,29 +20,34 @@ import org.json.simple.parser.JSONParser;
 
 public class World {
 	private final int UNINITIALIZED = -1;
-	
+
 	private Tile[][] tiles;
-	private Building[] buildings;
+	private ArrayList<Building> buildings;
 	private LinkedList<Updateable> updateables;
 	private LinkedList<Drawable> drawables;
 	private LinkedList<Closable> toRemove;
-	private HashMap<String, Integer> buildingMap; // maps between the building and it's corresponding id
-	
-	private double realTimeToSimTime; // when a real time in ms is multiplied with this number,
-					//and the ceiling of the product is taken is will give the time in simulation time in seconds
+	private HashMap<String, Integer> buildingMap; // maps between the building
+													// and it's corresponding id
+
+	private double realTimeToSimTime; // when a real time in ms is multiplied
+										// with this number,
+	// and the ceiling of the product is taken is will give the time in
+	// simulation time in seconds
 	private int worldTime; // the current time in the simulation world
-	private int deltaTime; // the delta between the second to last update() and the latest update()
-	private long lastRealTime = UNINITIALIZED; // the last time update was called is used to calculate deltaTime
+	private int deltaTime; // the delta between the second to last update() and
+							// the latest update()
+	private long lastRealTime = UNINITIALIZED; // the last time update was
+												// called is used to calculate
+												// deltaTime
 	private double timeRemainder = 0;
-	
+
 	public final agenda.Agenda agenda;
-	
+
 	private Image mapImage;
-	
+
 	protected static World instance;
-	
-	public World()
-	{
+
+	public World() {
 		this.agenda = new agenda.Agenda();
 		updateables = new LinkedList<>();
 		drawables = new LinkedList<>();
@@ -49,9 +55,8 @@ public class World {
 		instance = this;
 		realTimeToSimTime = 0.1;
 	}
-	
-	public World(agenda.Agenda agenda, Map<agenda.Stage, Integer> stageMap, String jsonPath, String tileMapPath)
-	{
+
+	public World(agenda.Agenda agenda, Map<agenda.Stage, Integer> stageMap, String jsonPath, String tileMapPath) {
 		this.agenda = agenda;
 		
 		boolean[][] colisionInfo = null;
@@ -61,7 +66,9 @@ public class World {
 			JSONParser parser = new JSONParser();
 			int height;
 			int width;
+			buildings = new ArrayList<Building>();
 			ArrayList<TileLayer> layerslist = new ArrayList<>();
+			ArrayList<TileLayer> collisionlist = new ArrayList<>();
 
 			try {
 				// tilemap maken door een tilemap object aan te maken.
@@ -71,48 +78,112 @@ public class World {
 				Object file = parser.parse(new FileReader(jsonPath));
 				JSONObject json = (JSONObject) file;
 				JSONArray layers = (JSONArray) json.get("layers");
-				// hoogte + breedte van de hele map.
+				// hoogte + breedte van de hele map.`
 				height = ((Long) json.get("height")).intValue();
 				width = ((Long) json.get("width")).intValue();
-				
+
 				colisionInfo = new boolean[width][height];
 
 				// jsonfile uitlezen op layers en toevoegen als tilelayer in
 				// tilelayer arraylist.
-				// als de layer een visible : true heeft dan wordt hij toegevoegd
+				// als de layer een visible : true heeft dan wordt hij
+				// toegevoegd
 				// met de parameter true zodat de tekenmethode weet dat de layer
 				// moet worden getekend.
-				// dit is voor de collision layers : die hebben false in de JSON.
-				
-				//TODO nog editten naar wat binnenkomt kwa wat er gebruikt moet worden.
-				for (int i = 0; i < layers.size(); i++) {
-					JSONObject currentlayer = (JSONObject) layers.get(i);
-					if (currentlayer.get("type").equals("tilelayer")) {
-						boolean b = false;
-						if (currentlayer.get("visible").equals(true)) {
-							b = true;
+				// dit is voor de collision layers : die hebben false in de
+				// JSON.
+
+				Iterator itr = stageMap.entrySet().iterator();
+				for (Map.Entry<agenda.Stage, Integer> entry : stageMap.entrySet()) {
+					
+					agenda.Stage stage = entry.getKey();
+					int stageID = entry.getValue();
+
+					JSONObject currentlayer = (JSONObject) layers.get(stageID);
+					
+					TileLayer e = new TileLayer((JSONArray) currentlayer.get("data"), map, height, width, true);
+					layerslist.add(e);
+					
+					
+					
+					if (currentlayer.containsKey("properties")) {
+						
+						JSONArray drawproperties = (JSONArray) currentlayer.get("properties");
+						
+						for (int j = 0; j < drawproperties.size(); j++) {
+							
+							JSONObject layer = (JSONObject) layers.get((int) drawproperties.get(j));
+							
+							if (layer.get("visible").equals(true)) {
+								TileLayer temp = new TileLayer((JSONArray) layer.get("data"), map, height, width, true);
+								layerslist.add(temp);
+							}
+							else
+							{
+								TileLayer coll = new TileLayer((JSONArray) layer.get("data"), map, height, width, false);
+								collisionlist.add(coll);
+							}
+							
 						}
-						TileLayer e = new TileLayer((JSONArray) currentlayer.get("data"), map, height, width, b);
-						layerslist.add(e);
+
 					}
+
 				}
+
+				// for (int i = 0; i < layers.size(); i++) {
+				// //pak de layer
+				// JSONObject currentlayer = (JSONObject) layers.get(i);
+				// //kijk of er properties in zitten
+				//
+				// if(currentlayer.containsKey("properties"))
+				// {
+				//
+				// //pak de properties als een array
+				// JSONArray drawproperties = (JSONArray)
+				// currentlayer.get("properties");
+				// //ga door de array heen om die layers toe te voegen
+				// for(int j = 0; j < drawproperties.size(); i++)
+				// {
+				// boolean visible = true;
+				// JSONObject currentlayer = (JSONObject) layers.get(i);
+				//
+				// TileLayer e = new TileLayer((JSONArray)
+				// currentlayer.get("data"), map, height, width, visible);
+				// layerslist.add(e);
+				// }
+				//
+				// }
+
+				// if (currentlayer.get("type").equals("tilelayer")) {
+				// boolean b = false;
+				// if (currentlayer.get("visible").equals(true)) {
+				// b = true;
+				// }
+				// TileLayer e = new TileLayer((JSONArray)
+				// currentlayer.get("data"), map, height, width, b);
+				// layerslist.add(e);
+				// }
+				// }
+
 				// tileheight en width bepalen door uit de json te halen.
-				// width en tilewidth zijn verschillende waarde. width = groote van
+				// width en tilewidth zijn verschillende waarde. width = groote
+				// van
 				// de map en tilewidth is puur de tilewidth (32 bij ons..)
+				
 				mapImage = new BufferedImage(width * map.getTileWidth(), height * map.getTileHeight(),
 						BufferedImage.TYPE_INT_ARGB);
 
-				
-				//Collision toevoegen in een arraylist van booleans?
+				// Collision toevoegen in een arraylist van booleans?
 				// 2 tiles : tileID 90 en 86.
-				//op basis van collision later > tiled eigenschap!
-				
+				// op basis van collision later > tiled eigenschap!
+
 			} catch (Exception e) {
 				e.printStackTrace();
 
 			}
 
-			// tekenen op de map, dit gebeurt maar 1x en zorgt ervoor dat de map dus
+			// tekenen op de map, dit gebeurt maar 1x en zorgt ervoor dat de map
+			// dus
 			// niet meer veranderd maar wel gebruikt kan worden zonder veel
 			// geheugen.
 			try {
@@ -122,70 +193,65 @@ public class World {
 					g.drawImage(layer.getLayerImage(), 0, 0, null);
 				}
 				layerslist.clear();
-				// Garbage collecter notification voor java, om die tering rommel op
+				// Garbage collecter notification voor java, om die tering
+				// rommel op
 				// te ruimen >> 1500MB.
-				//TODO FIX JAVA.
+				// TODO FIX JAVA.
 				System.gc();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		{
 			{ // check info
-			if (colisionInfo == null)
-				return; // TODO exception
-			int lastLenght = colisionInfo[0].length;
-			for (boolean[] boolArray : colisionInfo)
-				if (boolArray.length != lastLenght)
-					return; // TODO exception?
+				if (colisionInfo == null)
+					return; // TODO exception
+				int lastLenght = colisionInfo[0].length;
+				for (boolean[] boolArray : colisionInfo)
+					if (boolArray.length != lastLenght)
+						return; // TODO exception?
 			}
-			
-			class Node
-			{
+
+			class Node {
 				final int X, Y;
 				Node[] straitEdges;
 				Node[] diagonalEdges;
-				
-				public Node(int x, int y)
-				{
+
+				public Node(int x, int y) {
 					X = x;
 					Y = y;
 					straitEdges = new Node[4];
 					diagonalEdges = new Node[4];
 				}
-				
+
 				@Override
-				public int hashCode()
-				{
-					int hash = 89*X+7*Y;
-					for (int i = 0; i < straitEdges.length; i++)
-					{
+				public int hashCode() {
+					int hash = 89 * X + 7 * Y;
+					for (int i = 0; i < straitEdges.length; i++) {
 						if (straitEdges[i] != null)
-							hash = hash*89+straitEdges[i].X+7*straitEdges[i].Y*7;
+							hash = hash * 89 + straitEdges[i].X + 7 * straitEdges[i].Y * 7;
 						else
 							continue;
 					}
-					for (int i = 0; i < diagonalEdges.length; i++)
-					{
+					for (int i = 0; i < diagonalEdges.length; i++) {
 						if (diagonalEdges[i] != null)
-							hash = hash*89+diagonalEdges[i].X+7*diagonalEdges[i].Y*7;
+							hash = hash * 89 + diagonalEdges[i].X + 7 * diagonalEdges[i].Y * 7;
 						else
 							continue;
 					}
 					return hash;
 				}
-				
+
 				@Override
-				public boolean equals(Object o)
-				{
+				public boolean equals(Object o) {
 					if (o instanceof Node == false)
 						return false;
-					Node n = (Node)o;
+					Node n = (Node) o;
 					return X == n.X && Y == n.Y;
 				}
 			}
-			
+
 			ArrayList<Node> graph;
 			{ ///////////// Generating the graph ///////////
 				int nodeCount = 0;
@@ -194,211 +260,190 @@ public class World {
 						if (bool)
 							nodeCount++;
 				graph = new ArrayList<>(nodeCount);
-				
-				class Position
-				{
+
+				class Position {
 					int x, y;
-					public Position(int x, int y)
-					{
+
+					public Position(int x, int y) {
 						this.x = x;
 						this.y = y;
 					}
-					public void set(int x, int y)
-					{
+
+					public void set(int x, int y) {
 						this.x = x;
 						this.y = y;
 					}
-					
+
 					@Override
-					public int hashCode()
-					{
-						return 111*x+7*y;
+					public int hashCode() {
+						return 111 * x + 7 * y;
 					}
-					
+
 					@Override
-					public boolean equals(Object e)
-					{
+					public boolean equals(Object e) {
 						if (e instanceof Position == false)
 							return false;
-						Position p = (Position)e;
+						Position p = (Position) e;
 						return x == p.x && y == p.y;
 					}
 				}
-				
+
 				HashMap<Position, Node> positionToNodeMap = new HashMap<>(nodeCount);
-				
+
 				for (int y = 0; y < colisionInfo.length; y++)
 					for (int x = 0; x < colisionInfo[0].length; x++)
-						if (colisionInfo[y][x] == true)
-						{
+						if (colisionInfo[y][x] == true) {
 							Node n = new Node(x, y);
 							graph.add(n);
 							positionToNodeMap.put(new Position(x, y), n);
 						}
-				
-				for (Node n : graph)
-				{ // adding the edges to the node TODO diagonal?
+
+				for (Node n : graph) { // adding the edges to the node TODO
+										// diagonal?
 					int index = 0;
-					Position position = new Position(n.X-1, n.Y);
-					if (positionToNodeMap.containsKey(position))
-					{
+					Position position = new Position(n.X - 1, n.Y);
+					if (positionToNodeMap.containsKey(position)) {
 						n.straitEdges[index] = positionToNodeMap.get(position);
 						index++;
 					}
-					
-					position.set(n.X+1, n.Y);
-					if (positionToNodeMap.containsKey(position))
-					{
+
+					position.set(n.X + 1, n.Y);
+					if (positionToNodeMap.containsKey(position)) {
 						n.straitEdges[index] = positionToNodeMap.get(position);
 						index++;
 					}
-					
-					position.set(n.X, n.Y-1);
-					if (positionToNodeMap.containsKey(position))
-					{
+
+					position.set(n.X, n.Y - 1);
+					if (positionToNodeMap.containsKey(position)) {
 						n.straitEdges[index] = positionToNodeMap.get(position);
 						index++;
 					}
-					
-					position.set(n.X, n.Y+1);
-					if (positionToNodeMap.containsKey(position))
-					{
+
+					position.set(n.X, n.Y + 1);
+					if (positionToNodeMap.containsKey(position)) {
 						n.straitEdges[index] = positionToNodeMap.get(position);
 						index++;
 					}
-					
-					
+
 				}
-				
+
 			} ////////////// End of graph generation ///////////
 
 			{ ////////////// Graph test using depth first search /////////
 				Stack<Node> nodeStack = new Stack<>();
 				HashSet<Node> visitedNodes = new HashSet<>(graph.size());
-				
+
 				nodeStack.push(graph.get(0));
 				visitedNodes.add(nodeStack.peek());
-				
-				while (nodeStack.empty() == false)
-				{
+
+				while (nodeStack.empty() == false) {
 					Node n = nodeStack.peek();
 					visitedNodes.add(n);
 					boolean pushed = false;
-					
-					for (int i = 0; i < n.straitEdges.length; i++)
-					{
+
+					for (int i = 0; i < n.straitEdges.length; i++) {
 						if (n.straitEdges[i] == null || visitedNodes.contains(n.straitEdges[i]))
 							continue;
 						nodeStack.push(n.straitEdges[i]);
 						pushed = true;
 						break;
 					}
-					
+
 					if (pushed == false)
 						nodeStack.pop();
 				}
-				
-				if (visitedNodes.size() != graph.size())
-				{
-					System.out.println("not all nodes are conected"); // TODO exception
+
+				if (visitedNodes.size() != graph.size()) {
+					System.out.println("not all nodes are conected"); // TODO
+																		// exception
 					return;
 				}
 			} ////////// end of graph test //////////////////////////
-			
+
 			{ ////////// generating directions /////////////////////
 				int startX = 0, startY = 0;
-				
-				
+
 			}
 		}
+
 	}
-	
-	public void inclusiveUpdate(Graphics2D g2)
-	{
+
+	public void inclusiveUpdate(Graphics2D g2) {
 		update();
 		draw(g2);
 		cleanUp();
 	}
-	
+
 	// this method has been checked and it works correctly 14/3/2016
-	public void update()
-	{
+	public void update() {
 		{// update everything related to time
 			long realTime = System.currentTimeMillis();
 			System.out.println(timeRemainder);
 			if (lastRealTime == UNINITIALIZED)
 				lastRealTime = realTime;
-			double deltaTimeDouble = realTime-lastRealTime+timeRemainder;
-			deltaTime = (int)(deltaTimeDouble*realTimeToSimTime);
-			timeRemainder = (deltaTimeDouble*realTimeToSimTime-deltaTime)/realTimeToSimTime;
-			
+			double deltaTimeDouble = realTime - lastRealTime + timeRemainder;
+			deltaTime = (int) (deltaTimeDouble * realTimeToSimTime);
+			timeRemainder = (deltaTimeDouble * realTimeToSimTime - deltaTime) / realTimeToSimTime;
+
 			worldTime += deltaTime;
 			lastRealTime = realTime;
 		}
-		
+
 		{// update all updatables
 			ListIterator<Updateable> iterator = updateables.listIterator();
 			while (iterator.hasNext())
 				iterator.next().update();
 		}
 	}
-	
-	public void cleanUp()
-	{
+
+	public void cleanUp() {
 		ListIterator<Closable> iterator = toRemove.listIterator();
 		while (iterator.hasNext())
 			iterator.next().close();
-		
+
 		toRemove.clear();
 	}
 
-	public void draw(Graphics2D graphics)
-	{
+	public void draw(Graphics2D graphics) {
 		graphics.drawImage(mapImage, new AffineTransform(), null);
-		
+
 		ListIterator<Drawable> iterator = drawables.listIterator();
 		while (iterator.hasNext())
 			iterator.next().draw(graphics);
 	}
-	
-	protected void regesterUpdateable(Updateable u) 
-	{
+
+	protected void regesterUpdateable(Updateable u) {
 		updateables.add(u);
 	}
-	protected void unregesterUpdatable(Updateable u) 
-	{
+
+	protected void unregesterUpdatable(Updateable u) {
 		updateables.remove(u);
 	}
-	
-	protected void regesterDrawable(Drawable d) 
-	{
+
+	protected void regesterDrawable(Drawable d) {
 		drawables.add(d);
 	}
-	protected void unregesterDrawable(Drawable d)
-	{
+
+	protected void unregesterDrawable(Drawable d) {
 		drawables.remove(d);
 	}
-	
-	protected enum PathInfo
-	{
+
+	protected enum PathInfo {
 		ENTRANCE, EXIT
 	}
-	
-	protected int getPathID(String building, PathInfo info)
-	{
+
+	protected int getPathID(String building, PathInfo info) {
 		if (buildingMap.containsKey(building) == false)
 			return -1;
 		return buildingMap.get(building) << 1 | ((info == PathInfo.ENTRANCE) ? 0 : 1);
 	}
-	
-	protected int getWorldTime()
-	{
+
+	protected int getWorldTime() {
 		return worldTime;
 	}
-	
-	protected int getDeltaTime()
-	{
+
+	protected int getDeltaTime() {
 		return deltaTime;
 	}
-	
+
 }
