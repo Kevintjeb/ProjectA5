fe28696b9e7cfcf340d18d7e54eba63e2bdbb356
mcvoogd <mcvoogd@avans.nl>
2016-03-31 15:14:03 +0200

    commit Linking fixed en maps map outside map code

--- code/agenda/Linking.java
+++ code/agenda/Linking.java
@@ -15,339 +15,197 @@ import java.util.HashMap;
 
 import javax.swing.JPanel;
 
-public class Linking extends JPanel
-{
+public class Linking extends JPanel {
 	Planner planner;
 	Font font = new Font("SANS_SERIF", Font.PLAIN, 14);
 	HashMap<Stage, Integer> map = new HashMap<Stage, Integer>();
-		
+
 	ArrayList<Rectangle2D> stages = new ArrayList<Rectangle2D>();
 	ArrayList<Rectangle2D> numbers = new ArrayList<Rectangle2D>();
-		
+
 	Rectangle2D rectSelected;
 	Rectangle2D rectNumSelected;
-		
+
 	Stage selectedStage;
 	int selectedNumber;
-		
+
+	int clickedStage = -1;
+	int clickedNumber = -1;
+
 	Rectangle2D applyButton;
 	Rectangle2D diposeButton;
-		
+
 	boolean drawLine = false;
 	boolean hasBeenClicked = false;
-	
+
 	File json;
-		
-<<<<<<< HEAD
-		int clickedStage = -1;
-		int clickedNumber = -1;
-		
-		Rectangle2D applyButton;
-		Rectangle2D diposeButton;
-=======
->>>>>>> efb54cdb22b353433e1315d4d47635b96392cc34
-		
-		
-	public Linking(Planner planner, File json)
-	{
+
+	public Linking(Planner planner, File json) {
 		super(null);
 		this.planner = planner;
 		this.json = json;
 		setPreferredSize(new Dimension(400, 550));
 		clicked();
 	}
-		
-	public void paintComponent(Graphics g)
-	{
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
 		int y = 10;
 		int y2 = 10;
-		
-		if(!stages.isEmpty())
-		{
+		if (!stages.isEmpty()) {
 			stages.clear();
 		}
-		if(!numbers.isEmpty())
-		{
+		if (!numbers.isEmpty()) {
 			numbers.clear();
 		}
-		
-		super.paintComponents(g);
-		Graphics2D g2d = (Graphics2D) g;
-		
-		for(Stage stage : planner.agenda.getStages())
-		{
-<<<<<<< HEAD
-			super.paintComponent(g);
-			Graphics2D g2d = (Graphics2D) g;
-			int y = 10;
-			int y2 = 10;
-			System.out.println(!stages.isEmpty());
-			System.out.println(!numbers.isEmpty());
-			if(!stages.isEmpty())
-			{
-				stages.clear();
-			}
-			if(!numbers.isEmpty())
-			{
-				numbers.clear();
-			}			
-			
-			for(int stage = 0; stage < planner.agenda.getStages().size(); stage++)
-			{
-			
-				if(stage == clickedStage){
-					g2d.setColor(new Color(25, 160, 95, 255));
-					System.out.println(stage);}
-				else	
-					g2d.setColor(new Color(25, 160, 95, 192));
-				
-					Rectangle2D rect = new Rectangle2D.Double(10, y, 100, 50);
-					g2d.fill(rect);
-					
-					stages.add(rect);
-					
-					int stringLen = (int) g2d.getFontMetrics().getStringBounds(planner.agenda.getStages().get(stage).getName(), g2d).getWidth();
-					int stringHei = (int) g2d.getFontMetrics().getStringBounds(planner.agenda.getStages().get(stage).getName(), g2d).getHeight();
-					
-					int xPos = (int) 100 / 2 - stringLen / 2;
-					int yPos = (int) 50 / 2 + stringHei / 4;
-					
-					g2d.setColor(Color.BLACK);
-					g2d.drawString(planner.agenda.getStages().get(stage).getName(), xPos + 10, (yPos + y));
-					
-					y += 60;
-					g2d.setColor(new Color(25, 160, 95, 128));
-			}
-			
-			int count = 0;
-			for(int i = 6; i <= 27; i = i+3)
-			{
-				if(count == clickedNumber){
-					g2d.setColor(new Color(225, 230, 150, 255));
-					System.out.println(i);}
-				else
-					g2d.setColor(new Color(225, 230, 150, 192));
-				count++;
-				
-					Rectangle2D rect = new Rectangle2D.Double(290, y2, 100, 50);
-					g2d.fill(rect);
-=======
-			if(hasBeenClicked){
+
+		for (int stage = 0; stage < planner.agenda.getStages().size(); stage++) {
+
+			if (stage == clickedStage) {
+				g2d.setColor(new Color(25, 160, 95, 255));
+				System.out.println(stage);
+			} else
 				g2d.setColor(new Color(25, 160, 95, 192));
-				Rectangle2D rect = new Rectangle2D.Double(10, y, 100, 50);
-				g2d.fill(rect);
-			
-				stages.add(rect);
-				
-				int stringLen = (int) g2d.getFontMetrics().getStringBounds(stage.getName(), g2d).getWidth();
-				int stringHei = (int) g2d.getFontMetrics().getStringBounds(stage.getName(), g2d).getHeight();
-			
-				int xPos = (int) 100 / 2 - stringLen / 2;
-				int yPos = (int) 50 / 2 + stringHei / 4;				
-					
-				g2d.setColor(Color.BLACK);
-				g2d.drawString(stage.getName(), xPos + 10, (yPos + y));
-			
-				y += 60;
-			}
-			else{
-				g2d.setColor(new Color(25, 160, 95, 128));
-				Rectangle2D rect = new Rectangle2D.Double(10, y, 100, 50);
-				g2d.fill(rect);
->>>>>>> efb54cdb22b353433e1315d4d47635b96392cc34
-				
-				stages.add(rect);
-				
-				int stringLen = (int) g2d.getFontMetrics().getStringBounds(stage.getName(), g2d).getWidth();
-				int stringHei = (int) g2d.getFontMetrics().getStringBounds(stage.getName(), g2d).getHeight();
-				
-				int xPos = (int) 100 / 2 - stringLen / 2;
-				int yPos = (int) 50 / 2 + stringHei / 4;
-				
-				g2d.setColor(Color.BLACK);
-				g2d.drawString(stage.getName(), xPos + 10, (yPos + y));
-				
-<<<<<<< HEAD
-					y2 += 60;
-					g2d.setColor(new Color(225, 230, 150, 128));
-=======
-				y += 60;
->>>>>>> efb54cdb22b353433e1315d4d47635b96392cc34
-			}
+
+			Rectangle2D rect = new Rectangle2D.Double(10, y, 100, 50);
+			g2d.fill(rect);
+
+			stages.add(rect);
+
+			int stringLen = (int) g2d.getFontMetrics()
+					.getStringBounds(planner.agenda.getStages().get(stage).getName(), g2d).getWidth();
+			int stringHei = (int) g2d.getFontMetrics()
+					.getStringBounds(planner.agenda.getStages().get(stage).getName(), g2d).getHeight();
+
+			int xPos = (int) 100 / 2 - stringLen / 2;
+			int yPos = (int) 50 / 2 + stringHei / 4;
+
+			g2d.setColor(Color.BLACK);
+			g2d.drawString(planner.agenda.getStages().get(stage).getName(), xPos + 10, (yPos + y));
+
+			y += 60;
+			g2d.setColor(new Color(25, 160, 95, 128));
 		}
-			
-		for(int i = 6; i <= 27; i = i+3)
-		{
-			if(hasBeenClicked){
+
+		int count = 0;
+		for (int i = 6; i <= 27; i = i + 3) {
+			if (count == clickedNumber) {
+				g2d.setColor(new Color(225, 230, 150, 255));
+				System.out.println(i);
+			} else
 				g2d.setColor(new Color(225, 230, 150, 192));
-				Rectangle2D rect = new Rectangle2D.Double(290, y2, 100, 50);
-				g2d.fill(rect);
-				
-				numbers.add(rect);
-			
-				int stringLen = (int) g2d.getFontMetrics().getStringBounds(""+i, g2d).getWidth();
-				int stringHei = (int) g2d.getFontMetrics().getStringBounds(""+i, g2d).getHeight();
-				
-				int xPos = (int) 100 / 2 - stringLen / 2;
-				int yPos = (int) 50 / 2 + stringHei / 4;
-			
-				g2d.setColor(Color.BLACK);
-				g2d.drawString(""+i, xPos + 290, (yPos + y2));
-			
-				y2 += 60;
-			}
-			else{
-				g2d.setColor(new Color(225, 230, 150, 128));
-				Rectangle2D rect = new Rectangle2D.Double(290, y2, 100, 50);
-				g2d.fill(rect);
-			
-				numbers.add(rect);
-				
-				int stringLen = (int) g2d.getFontMetrics().getStringBounds(""+i, g2d).getWidth();
-				int stringHei = (int) g2d.getFontMetrics().getStringBounds(""+i, g2d).getHeight();
-			
-				int xPos = (int) 100 / 2 - stringLen / 2;
-				int yPos = (int) 50 / 2 + stringHei / 4;
-			
-				g2d.setColor(Color.BLACK);
-				g2d.drawString(""+i, xPos + 290, (yPos + y2));
-			
-				y2 += 60;
-			}
+			count++;
+
+			Rectangle2D rect = new Rectangle2D.Double(290, y2, 100, 50);
+			g2d.fill(rect);
+
+			numbers.add(rect);
+
+			int stringLen = (int) g2d.getFontMetrics().getStringBounds("" + i, g2d).getWidth();
+			int stringHei = (int) g2d.getFontMetrics().getStringBounds("" + i, g2d).getHeight();
+
+			int xPos = (int) 100 / 2 - stringLen / 2;
+			int yPos = (int) 50 / 2 + stringHei / 4;
+
+			g2d.setColor(Color.BLACK);
+			g2d.drawString("" + i, xPos + 290, (yPos + y2));
+
+			y2 += 60;
+			g2d.setColor(new Color(225, 230, 150, 192));
 		}
-		
+
 		g2d.setColor(new Color(121, 121, 121));
 		applyButton = new Rectangle2D.Double(80, 500, 110, 50);
 		g2d.fill(applyButton);
-		
+
 		int stringLen = (int) g2d.getFontMetrics().getStringBounds("APPLY", g2d).getWidth();
 		int stringHei = (int) g2d.getFontMetrics().getStringBounds("APPLY", g2d).getHeight();
-		
+
 		int xPos = (int) 75 / 2 - stringLen / 2;
 		int yPos = (int) 50 / 2 + stringHei / 4;
-		
+
 		g2d.setColor(Color.WHITE);
 		g2d.drawString("APPLY", xPos + 100, (yPos + 500));
-		//--------------------------------------------------------------------------
+		// --------------------------------------------------------------------------
 		g2d.setColor(new Color(121, 121, 121));
 		diposeButton = new Rectangle2D.Double(210, 500, 110, 50);
 		g2d.fill(diposeButton);
-		
+
 		int stringLenPotty = (int) g2d.getFontMetrics().getStringBounds("Close linker", g2d).getWidth();
 		int stringHeiPotty = (int) g2d.getFontMetrics().getStringBounds("Close linker", g2d).getHeight();
-		
+
 		int xPosPotty = (int) 75 / 2 - stringLenPotty / 2;
 		int yPosPotty = (int) 50 / 2 + stringHeiPotty / 4;
-		
+
 		g2d.setColor(Color.WHITE);
 		g2d.drawString("Close linker", xPosPotty + 227, (yPosPotty + 500));
-		
-		if(drawLine)
-		{
+
+		if (drawLine) {
 			int lx = (int) rectSelected.getX() + 100;
 			int ly = (int) rectSelected.getY() + 25;
 			int rx = (int) rectNumSelected.getX();
 			int ry = (int) rectNumSelected.getY() + 25;
-			
+
 			g2d.setColor(Color.BLACK);
 			g2d.setStroke(new BasicStroke(5));
 			g2d.drawLine(lx, ly, rx, ry);
 			drawLine = false;
-			
+
 		}
 	}
-	
-	public void clicked()
-	{
-		addMouseListener(new MouseAdapter()
-		{
-			public void mousePressed(MouseEvent e)
-			{
-				for(int i =0; i<stages.size(); i++)
-				{
-					if(stages.get(i).contains(e.getPoint()))
-					{
-<<<<<<< HEAD
-						if(stages.get(i).contains(e.getPoint()))
-						{
-							System.out.println("selected stage " + i);
-							selectedStage = planner.agenda.getStages().get(i);
-							rectSelected = stages.get(i);
-							clickedStage = i;
-							toggleHasBeenClicked();
-							repaint();
-						}
-=======
+
+	public void clicked() {
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				for (int i = 0; i < stages.size(); i++) {
+					if (stages.get(i).contains(e.getPoint())) {
+						System.out.println("selected stage " + i);
 						selectedStage = planner.agenda.getStages().get(i);
 						rectSelected = stages.get(i);
-						toggleHasBeenClicked();
+						clickedStage = i;
 						repaint();
-						toggleHasBeenClicked();
->>>>>>> efb54cdb22b353433e1315d4d47635b96392cc34
 					}
+
 				}
-				
-				for(int i=0; i<numbers.size(); i++)
-				{
-					if(numbers.get(i).contains(e.getPoint()))
-					{
-<<<<<<< HEAD
-						if(numbers.get(i).contains(e.getPoint()))
-						{
-							selectedNumber = 6 + (i*3);
-							rectNumSelected = numbers.get(i);
-							clickedNumber = i;
-							toggleHasBeenClicked();
-							repaint();
-						}
-=======
-						selectedNumber = 6 + (i*3);
+
+				for (int i = 0; i < numbers.size(); i++) {
+					if (numbers.get(i).contains(e.getPoint())) {
+						selectedNumber = 6 + (i * 3);
 						rectNumSelected = numbers.get(i);
-						toggleHasBeenClicked();
+						clickedNumber = i;
 						repaint();
-						toggleHasBeenClicked();
->>>>>>> efb54cdb22b353433e1315d4d47635b96392cc34
 					}
+
 				}
 			}
 		});
-		
-		addMouseListener(new MouseAdapter()
-		{
-			public void mousePressed(MouseEvent e)
-			{
-				if(applyButton.contains(e.getPoint()))
-				{
+
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (applyButton.contains(e.getPoint())) {
 					map.put(selectedStage, selectedNumber);
 					System.out.println(map.entrySet());
-					
+
 					drawLine = true;
 					repaint();
 				}
 			}
 		});
-		
-		addMouseListener(new MouseAdapter()
-		{
-			public void mousePressed(MouseEvent e)
-			{
-				if(diposeButton.contains(e.getPoint()))
-				{
+
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (diposeButton.contains(e.getPoint())) {
 					planner.tabbedPane.removeTabAt(2);
-					planner.tabbedPane.addTab("Simulatie", new Simulator(json, planner, map));//, linking.getLinkMap()));
+					planner.tabbedPane.addTab("Simulatie", new Simulator(json, planner, map));
 					planner.repaint();
-			        planner.revalidate();
+					planner.revalidate();
 				}
 			}
 		});
 	}
-		
-	private void toggleHasBeenClicked(){
-		hasBeenClicked = !hasBeenClicked;
-	}
-	
-	public HashMap<Stage, Integer> getLinkMap()
-	{
+
+	public HashMap<Stage, Integer> getLinkMap() {
 		return map;
 	}
 }
